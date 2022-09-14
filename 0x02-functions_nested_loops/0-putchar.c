c5bdcad0e1cc853d9a88b248e264147590842da8
Jaymeskenny <jaymesrachael@gmail.com>
2022-09-14 12:23:27 +0000

    Nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
- 		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
+	_putchar('_');
+	_putchat('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-		return(0);
+	return(0);
 }
