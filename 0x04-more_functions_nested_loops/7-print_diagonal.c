e30780a60328d12a4949a492281a3bfb1f9edffd
Jaymeskenny <jaymesrachael@gmail.com>
2022-09-16 20:34:36 +0000

    More Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == 1)
+				if (j == i)
 					_putchar('\\');
 				else if (j < i)
 					_putchar(' ');
@@ -26,4 +26,4 @@ void print_diagonal(int n)
 			_putchar('\n');
 		}
 	}
-}
+i}
