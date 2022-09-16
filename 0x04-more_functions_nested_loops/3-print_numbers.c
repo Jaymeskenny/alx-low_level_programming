4e94fa5bcafcc9d62fae44f6a1ad6aa8a3625156
Jaymeskenny <jaymesrachael@gmail.com>
2022-09-16 10:06:40 +0000

    More Functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "nain.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+void print_numbers(void)
+{
+	int u;
+
+	for (u = 48; u < 58; u++ )
+	{
+		_putchar(u);
+	}
+	_putchar(10);
+}
