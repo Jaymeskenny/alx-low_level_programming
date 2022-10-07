65273c0ee2750671dd184dd50a3af89f51b24b04
Jaymeskenny <jaymesrachael@gmail.com>
2022-10-07 11:55:01 +0000

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
  */
 int sqrt2(int a, int b)
 {
-	if ( b * b == a)
+	if (b * b == a)
 		return (b);
 	else if (b * b > a)
 		return (-1);
@@ -23,5 +23,5 @@ int sqrt2(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n , 1));
+	return (sqrt2(n, 1));
 }
