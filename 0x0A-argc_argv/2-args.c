2cfef51d9900e5df748ecc539bdfc5bd89c2337c
Jaymeskenny <jaymesrachael@gmail.com>
2022-10-07 15:18:13 +0000

    Argc and Argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 /**
  * main - entry point
  * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containig strings entering main
+ * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 
 	if (argc > 0)
 	{
-		for (i = 0; i < argc: i++)
+		for (i = 0; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
 		}
