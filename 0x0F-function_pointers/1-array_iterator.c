c801498adf9aa315544cf68d0272d84e87195a4c
Jaymeskenny <jaymesrachael@gmail.com>
2022-10-12 18:47:06 +0000

    Function Pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (!array || !action)
 		return;
-
-			for (i = 0; i < size; i++)
-				action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
